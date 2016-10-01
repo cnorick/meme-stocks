881f0aee6548e646709b58d09abd61275d197927
Jacob Allen <jacoballen@Jacobs-MacBook-Air-3.local>
2016-10-01 11:31:21 -0400

    Add Tables for MVP

---
+++ backend.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Memes
+(
+memeID int not null PRIMARY KEY identity,
+memeName nvarchar(max),
+memePicture varbinary(max)
+);
+
+CREATE TABLE memeStats
+(
+memeStatsID int not null PRIMARY KEY identity,
+memeID int,
+timestamp datetime2,
+score int,
+foreign key(memeID) references memes(memeID)
+);
