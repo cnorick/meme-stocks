879ba635e8b291e18a4a18910cdf2c164ce79b40
osillyscope <brandoncb95@gmail.com>
2016-10-01 17:32:58 -0400

    Made parameters work.

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 #!webapi/bin/python
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pyodbc
+
+
+class memestat:
+	
+	pass 
+	
+
+
+
+
+
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -13,29 +24,35 @@ def index():
     return app.send_static_file('index.html')
 
 
+
+
 @app.route('/<path:path>')
 def send_js(path):
     return app.send_static_file(path)
 
 
+
+
 # This is the endpoint that returns all of the memes from the database.
+#
 @app.route('/api/memes', methods=['GET'])
-def get_meme():
+def getMeme(start, end):
     # Once Jacob gets the database setup, this function will get data from it.
     # Right now, though, we'll just use fake data.
-    memes = [
-        {
-            'id': 1,
-            'name': u'Harambe',
-            'image': u'Harambe image', 
-        },
-        {
-            'id': 2,
-            'name': u'Pepe',
-            'image': u'Pepe image', 
-        },
-    ]
-    return jsonify({'memes': memes})
+    pass
+
+
+
+
+@app.route('/api/MemeStats',  methods=['GET'])
+def getAllMemes():
+	start = request.args.get('start')
+	end = request.args.get('end')
+	return jsonify({'start' : start, 'end':end})	
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
