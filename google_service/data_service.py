c589f21fbed83cd656becc1df0935495698ebfc2
Trevor Sharpe <doublerazr@Trevors-MacBook-Air.local>
2016-10-02 02:21:42 -0400

    added functionality to specify date

--- google_service/data_service.py
+++ google_service/data_service.py
@@ -2,6 +2,7 @@
 from pytrends.request import TrendReq
 import sys
 import time
+from datetime import datetime
 from googleapiclient.discovery import build
 
 meme_list = ["good guy greg", "grumpy cat", "bad luck brian", "business cat", "batman slap robin",
@@ -23,7 +24,8 @@ def get_result_count(meme_name):
 
 #this function uses the product of the google search results count and the relative popularity
 #of the string observed from 2011 to 2016
-def get_score(meme_name):
+#meme_date must be the exact first day of a month if specified and in ISO format
+def get_score(meme_name, meme_date = None):
     maxvalue = get_result_count(meme_name)
     google_username = "danqjones@gmail.com"
     google_password = "volhacks"
@@ -33,20 +35,25 @@ def get_score(meme_name):
     trend_payload = {'q': meme_name, 'date': '01/2011 69m'}
 
     trend = pytrend.trend(trend_payload, return_type = 'dataframe')
-    time.sleep(10)
-    print(trend)
-    date = trend.index[-1]
+    time.sleep(1)
+    #if you want to see all the data retrieved uncomment the line below
+    #print(trend)
+    if meme_date is None:
+        date = trend.index[-1]
+    else:
+        date = datetime.strptime(meme_date, '%Y-%m-%d')
     score = trend.at[date,meme_name] * int(maxvalue)
     score = score / float(100)
     print str(date) + " " + str(score)
     return score
 
 #this method finds the highest score for the current month
-def get_high_score():
+#meme_date must be the exact first day of a month if specified and in ISO format
+def get_high_score(meme_date = None):
     maximum = 0
     max_meme = ""
     for meme in meme_list:
-        meme_score = get_score(meme)
+        meme_score = get_score(meme, meme_date)
         if meme_score > maximum:
             maximum = meme_score
             max_meme = meme
